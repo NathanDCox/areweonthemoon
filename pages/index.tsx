a561cb80a73fb63cc13a33e7b2aa75dd54938494
Nathan Cox <nathan.cox@kochgs.com>
2022-12-09 10:56:20 -0600

    head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 import ElapsedTime from '../components/ElapsedTime';
 
@@ -9,6 +10,10 @@ interface Props {
 const Home: React.FC<Props> = () => {
 	return (
 		<div className="container">
+			<Head>
+				<title>Are we on the Moon?</title>
+				<meta name="description" content="Are we on the Moon?" />
+			</Head>
 			<h1>Are we on the Moon?</h1>
 			<p className="declarative">NO</p>
 			<ElapsedTime date={new Date(Date.UTC(1972, 11, 14, 22, 54, 37))} />
