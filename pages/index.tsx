e61ece79486c91502325b73090c98c52ac237a3a
Nathan Cox <nathan.cox@kochgs.com>
2022-12-09 12:24:28 -0600

    analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Script from 'next/script';
 
 import ElapsedTime from '../components/ElapsedTime';
 
@@ -14,6 +15,16 @@ const Home: React.FC<Props> = () => {
 				<title>Are we on the Moon?</title>
 				<meta name="description" content="Are we on the Moon?" />
 			</Head>
+			<Script async src="https://www.googletagmanager.com/gtag/js?id=G-8NEYTLTCPL"></Script>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+
+					gtag('config', 'G-8NEYTLTCPL');
+				`}
+			</Script>
 			<h1>Are we on the Moon?</h1>
 			<p className="declarative">NO</p>
 			<ElapsedTime date={new Date(Date.UTC(1972, 11, 14, 22, 54, 37))} />
